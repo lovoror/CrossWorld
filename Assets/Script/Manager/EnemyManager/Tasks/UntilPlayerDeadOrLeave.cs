941821367471ad7f2625a87affbbf477095e576f
YuHuai <1030991501qq.com>
2018-07-04 23:35:13 +0800

    Enemy不断攻击Bug修复

--- Assets/Script/Manager/EnemyManager/Tasks/UntilPlayerDeadOrLeave.cs
+++ Assets/Script/Manager/EnemyManager/Tasks/UntilPlayerDeadOrLeave.cs
@@ -1,20 +1,25 @@
 ﻿using UnityEngine;
 namespace BehaviorDesigner.Runtime.Tasks.Basic.SharedVariables
 {
-	public class UntilPlayerDead : Action
+	public class UntilPlayerDeadOrLeave : Action
 	{
+		public SharedFloat attackDistance;
 		GameObject player;
 		Manager Player_Manager;
+		float sqAtkDist;
 		public override void OnStart()
 		{
 			base.OnStart();
+			
+			sqAtkDist = attackDistance.Value * attackDistance.Value;
 			player = (GameObject)Owner.GetVariable("Player").GetValue();
 			Player_Manager = player.GetComponent<Manager>();
 		}
 
 		public override TaskStatus OnUpdate()
 		{
-			if (Player_Manager.IsDead()) {
+			float sqDist = (transform.position - player.transform.position).sqrMagnitude;
+			if (Player_Manager.IsDead() || sqDist > sqAtkDist) {
 				return TaskStatus.Success;
 			}
 			else {
